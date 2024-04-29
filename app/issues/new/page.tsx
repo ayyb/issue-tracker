cb1f64532db4099266b10a7af9e35c56f2297860
Lee Myeong Seop <narabyeol@tobenix.co.kr>
2024-04-29 15:09:06 +0900

    마크다운 에디터 추가

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { TextArea, TextField,Button } from '@radix-ui/themes'
 import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
         <TextField.Root placeholder="title"/>
-        <TextArea placeholder="description"/>
+        <SimpleMDE  placeholder="description"/>
         <Button>Submit new Issue</Button>
     </div>
   )
