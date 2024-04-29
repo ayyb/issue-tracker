809afdce3c9849bd18fe7ade695a2941bc675522
Lee Myeong Seop <narabyeol@tobenix.co.kr>
2024-04-29 16:53:58 +0900

    리팩토링 : 이동 인라인 함수

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -32,6 +32,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
+ const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An uepected error occured");
+    }
+  })
+
 
   return (
     <div className="max-w-xl space-y-3">
@@ -42,16 +53,7 @@ const NewIssuePage = () => {
       )}
       <form
         className="max-w-xl space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An uepected error occured");
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root placeholder="title" {...register("title")} />
         {errors.title && <ErrorMessage>{errors.title?.message}</ErrorMessage>}
